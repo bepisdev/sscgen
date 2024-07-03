428e0f46cea4e2937fb94fb6279b147cf4b98b79
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2024-07-04 09:58:16 +1000

    init flags

--- cmd/sscgen/main.go
+++ cmd/sscgen/main.go
@@ -13,18 +13,22 @@ var (
 	rsaBits int
 	ecdsaCurve string
 	ed25519Key bool
+  printVersion bool
+  printUsage bool
 )
 
 
 // Initialize CLI flags
 func init() {
-  // TODO: host flag StringVarP
-  // TODO: validFrom flag StringVarP
-  // TODO: validFor flag DurationVarP
-  // TODO: isCA flag BoolVarP
-  // TODO: rsaBits flag IntVarP
-  // TODO: ecdsaCurve StringVarP
-  // TODO: ed25519Key BoolVarP
+  flag.StringVarP(&host, "host", "H", "", "Comma-separated hostnames and IPs to generate a certificate for")
+  flag.StringVarP(&validFrom, "start-date", "s", "", "Set creation date (formatted as Jan 1 15:04:05 2011)")
+  flag.DurationVarP(&validFor, "duration", 365*24*time.Hour, "Duration that certificate is valid for")
+  flag.BoolVarP(&isCA, "is-ca", "c", false, "Certificate is its own Certificate Authority")
+  flag.IntVarP(&ecdsaCurve, "rsa-bits", "b", 2048, "Size of RSA key to generate. Ignored if --ecdsa-curve is set")
+  flag.StringVarP(&ecdsaCurve, "ecdsa-curve", "e", "", "ECDSA curve to use to generate a key. Valid values are P224, P256 (recommended), P384, P521")
+  flag.BoolVar(&ed25519Key, "ed25519", false, "Generate an Ed25519 key")
+  flag.BoolVarP(&flagVersion, "version", "v", false, "Print version information")
+  flag.BoolVarP(&printUsage, "help", "h", false, "Print usage information")
 }
 
 func generatePublicKey(priv any) any {
