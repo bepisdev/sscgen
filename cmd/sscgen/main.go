def30cccfcbb7691260b5c64a5393b2d5743ddee
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2024-07-04 09:37:34 +1000

    Update main.go

--- cmd/sscgen/main.go
+++ cmd/sscgen/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+  flag "github.com/spf13/pflag"
+  "time"
+)
+
+var (
+	host string
+	validFrom string
+	validFor *time.Duration
+	isCA  bool
+	rsaBits int
+	ecdsaCurve string
+	ed25519Key bool
+)
+
+
+// Initialize CLI flags
+func init() {
+  // TODO: host flag StringVarP
+  // TODO: validFrom flag StringVarP
+  // TODO: validFor flag DurationVarP
+  // TODO: isCA flag BoolVarP
+  // TODO: rsaBits flag IntVarP
+  // TODO: ecdsaCurve StringVarP
+  // TODO: ed25519Key BoolVarP
+}
+
+func generatePublicKey(priv any) any {
+  // TODO: Generate public key  
+}
+
+func main() {}
