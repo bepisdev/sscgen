4b25a96de23128ae9d2d6c35e33bf6e3143c99ea
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2024-07-04 00:31:43 +0000

    document helper function

--- cmd/sscgen/helpers.go
+++ cmd/sscgen/helpers.go
@@ -6,6 +6,23 @@ import (
 	"crypto/rsa"
 )
 
+// publicKey returns the public key corresponding to the given private key.
+// It supports RSA, ECDSA, and Ed25519 private keys.
+//
+// Parameters:
+// - priv: The private key for which the corresponding public key needs to be obtained.
+//
+// Returns:
+// - The public key corresponding to the given private key.
+// - nil if the provided private key type is not supported.
+//
+// Example:
+//
+//	privKey, err := rsa.GenerateKey(rand.Reader, 2048)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	pubKey := publicKey(privKey)
 func publicKey(priv any) any {
 	switch k := priv.(type) {
 	case *rsa.PrivateKey:
