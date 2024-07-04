8a27a7fa1bb57051697cd40288e5721371263529
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2024-07-04 00:23:32 +0000

    fix typos in flags

--- cmd/sscgen/main.go
+++ cmd/sscgen/main.go
@@ -35,10 +35,10 @@ func init() {
 	flag.StringVarP(&validFrom, "start-date", "s", "", "Set creation date (formatted as Jan 1 15:04:05 2011)")
 	flag.DurationVarP(&validFor, "duration", 365*24*time.Hour, "Duration that certificate is valid for")
 	flag.BoolVarP(&isCA, "is-ca", "c", false, "Certificate is its own Certificate Authority")
-	flag.IntVarP(&ecdsaCurve, "rsa-bits", "b", 2048, "Size of RSA key to generate. Ignored if --ecdsa-curve is set")
+	flag.IntVarP(&rsaBits, "rsa-bits", "b", 2048, "Size of RSA key to generate. Ignored if --ecdsa-curve is set")
 	flag.StringVarP(&ecdsaCurve, "ecdsa-curve", "e", "", "ECDSA curve to use to generate a key. Valid values are P224, P256 (recommended), P384, P521")
 	flag.BoolVar(&ed25519Key, "ed25519", false, "Generate an Ed25519 key")
-	flag.BoolVarP(&flagVersion, "version", "v", false, "Print version information")
+	flag.BoolVarP(&printVersion, "version", "v", false, "Print version information")
 	flag.BoolVarP(&printUsage, "help", "h", false, "Print usage information")
 }
 
